32d01e8e3cea01940aa8e818c5d58a1ae90647fd
Ivan Dutov <dutovivan101@gmail.com>
2026-01-10 21:58:54 +0200

    build: final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
+import { resolve } from "path";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        $fonts: resolve("./src/assets/fonts"),
+      },
+    },
   },
 
   integrations: [react()],
